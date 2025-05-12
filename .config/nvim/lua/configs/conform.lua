4c008376e7c9474e2b07dcfd5c824eee9533b097
moucheh <moucheh@proton.me>
2025-05-13 00:09:00 +0200

    added clang-format to conform, added yaml-language-server to servers variable

--- .config/nvim/lua/configs/conform.lua
+++ .config/nvim/lua/configs/conform.lua
@@ -1,5 +1,9 @@
 local options = {
   formatters_by_ft = {
+    c = { 'clang-format' },
+    cpp = { 'clang-format' },
+    objc = { 'clang-format' },
+    objcpp = { 'clang-format' },
     lua = { 'stylua' },
     css = { 'prettierd', 'prettier', stop_after_first = true },
     html = { 'prettierd', 'prettier', stop_after_first = true },
--- .config/nvim/lua/configs/lspconfig.lua
+++ .config/nvim/lua/configs/lspconfig.lua
@@ -4,7 +4,7 @@ require('nvchad.configs.lspconfig').defaults()
 local lspconfig = require 'lspconfig'
 
 -- EXAMPLE
-local servers = { 'html', 'cssls', 'clangd', 'matlab_ls', 'emmet_language_server', 'vhdl_ls', 'ts_ls' }
+local servers = { 'html', 'cssls', 'clangd', 'matlab_ls', 'emmet_language_server', 'vhdl_ls', 'ts_ls', 'yamlls' }
 local nvlsp = require 'nvchad.configs.lspconfig'
 
 -- lsps with default config
