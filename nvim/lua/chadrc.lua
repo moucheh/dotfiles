c86796d50832f7aa9cc7e1cb3d30c43a079fc6ec
moucheh <moucheh@proton.me>
2025-06-17 22:17:43 +0200

    added lazygit keybind to nvdash

--- nvim/lua/chadrc.lua
+++ nvim/lua/chadrc.lua
@@ -26,6 +26,28 @@ M.nvdash = {
     ' ╚═╝     ╚═╝ ╚═════╝  ╚═════╝  ╚═════╝╚═╝  ╚═╝╚══════╝╚═╝  ╚═╝ ',
     '                                                               ',
   },
+  buttons = {
+    { txt = '  Find File', keys = 'ff', cmd = 'Telescope find_files' },
+    { txt = '  Recent Files', keys = 'fo', cmd = 'Telescope oldfiles' },
+    { txt = '󰈭  Find Word', keys = 'fw', cmd = 'Telescope live_grep' },
+    { txt = '󱥚  Themes', keys = 'th', cmd = ":lua require('nvchad.themes').open()" },
+    { txt = '  Mappings', keys = 'ch', cmd = 'NvCheatsheet' },
+    { txt = '  LazyGit', keys = 'lg', cmd = 'LazyGit' },
+
+    { txt = '─', hl = 'NvDashFooter', no_gap = true, rep = true },
+
+    {
+      txt = function()
+        local stats = require('lazy').stats()
+        local ms = math.floor(stats.startuptime) .. ' ms'
+        return '  Loaded ' .. stats.loaded .. '/' .. stats.count .. ' plugins in ' .. ms
+      end,
+      hl = 'NvDashFooter',
+      no_gap = true,
+    },
+
+    { txt = '─', hl = 'NvDashFooter', no_gap = true, rep = true },
+  },
 }
 
 M.ui = {
