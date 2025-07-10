90bf6d4b6a9ffb6a2ab235e5d4401f3055764334
moucheh <moucheh@proton.me>
2025-07-11 01:02:52 +0200

    adjusted player widget

--- waybar/mediaplayer.py
+++ waybar/mediaplayer.py
@@ -111,23 +111,19 @@ class PlayerManager:
     def on_metadata_changed(self, player, metadata, _=None):
         logger.debug(f"Metadata changed for player {player.props.player_name}")
         player_name = player.props.player_name
-        artist = player.get_artist()
         title = player.get_title()
         title = title.replace("&", "&amp;")
 
         track_info = ""
-        if player_name == "spotify" and "mpris:trackid" in metadata.keys() and ":ad:" in player.props.metadata["mpris:trackid"]:
-            track_info = "Advertisement"
-        elif artist is not None and title is not None:
-            track_info = f"{artist} - {title}"
-        else:
-            track_info = title
+        if title is not None:
+            track_info = f" {title}"
 
         if track_info:
             if player.props.status == "Playing":
                 track_info = " " + track_info
             else:
                 track_info = " " + track_info
+
         # only print output if no other player is playing
         current_playing = self.get_first_playing_player()
         if current_playing is None or current_playing.props.player_name == player.props.player_name:
