42cb6f75aa81bed6ce0b322581507de622483fac
moucheh <moucheh@proton.me>
2025-09-12 03:18:20 +0200

    the script now also calls stylua to format chadrc file

--- nvdash_art.sh
+++ nvdash_art.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
-figlet -f ~/.dotfiles/ansi_shadow.flf $1 > ascii_art.txt
+figlet -f ~/.dotfiles/ansi_shadow.flf $1 > /tmp/ascii_art.txt
 
-sed '1i\' ascii_art.txt
-sed -i "s/^/'/; s/\$/',/" ascii_art.txt
-sed -i '1i header = {\n' ascii_art.txt
-sed -i '$a },\n' ascii_art.txt
+sed '1i\' /tmp/ascii_art.txt
+sed -i "s/^/'/; s/\$/',/" /tmp/ascii_art.txt
+sed -i '1i header = {\n' /tmp/ascii_art.txt
+sed -i '$a },\n' /tmp/ascii_art.txt
 
 sed -i '/header = {/,/},/d' ~/.dotfiles/nvim/lua/chadrc.lua
 
-sed -i '/M.nvdash = {/r ascii_art.txt' ~/.dotfiles/nvim/lua/chadrc.lua
-rm ascii_art.txt
+sed -i '/M.nvdash = {/r /tmp/ascii_art.txt' ~/.dotfiles/nvim/lua/chadrc.lua
+rm /tmp/ascii_art.txt
+
+if command -v stylua >/dev/null 2>&1; then
+  stylua ~/.dotfiles/nvim/lua/chadrc.lua
+else
+  ~/.local/share/nvim/mason/bin/stylua ~/.dotfiles/nvim/lua/chadrc.lua
+fi
