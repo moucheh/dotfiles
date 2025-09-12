267fb8970aa5f1503190cd29b7655b6fc69b1015
moucheh <moucheh@proton.me>
2025-09-12 21:14:23 +0200

    adjusted nvdash_art script to use less permissions and added chadrc.lua backup before change

--- nvdash_art.sh
+++ nvdash_art.sh
@@ -1,21 +1,26 @@
 #!/bin/bash
-figlet -f ~/.dotfiles/ansi_shadow.flf $1 > /tmp/ascii_art.txt
+figlet -f ~/.dotfiles/ansi_shadow.flf $1 > ascii_art.txt
 
-sed -i '1i\' /tmp/ascii_art.txt
+sed -i '1i\' ascii_art.txt
 
-sed -i "s/^/' /; s/\$/ ',/" /tmp/ascii_art.txt
+sed -i "s/^/' /; s/\$/ ',/" ascii_art.txt
 
-sed -i '1i header = {' /tmp/ascii_art.txt
+sed -i '1i header = {' ascii_art.txt
 
-sed -i '$a },\n' /tmp/ascii_art.txt
+sed -i '$a },\n' ascii_art.txt
+
+cp ~/.dotfiles/nvim/lua/chadrc.lua ~/.dotfiles/nvim/lua/chadrc.lua.bak
+
+printf "\nchadrc.lua file backed up to ~/.dotfiles/nvim/chadrc.lua.bak\n"
 
 sed -i '/header = {/,/},/d' ~/.dotfiles/nvim/lua/chadrc.lua
 
-sed -i '/M.nvdash = {/r /tmp/ascii_art.txt' ~/.dotfiles/nvim/lua/chadrc.lua
-rm /tmp/ascii_art.txt
+sed -i '/M.nvdash = {/r ascii_art.txt' ~/.dotfiles/nvim/lua/chadrc.lua
 
 if command -v stylua >/dev/null 2>&1; then
   stylua ~/.dotfiles/nvim/lua/chadrc.lua
 else
-  ~/.local/share/nvim/mason/bin/stylua ~/.dotfiles/nvim/lua/chadrc.lua
+  if [ -f ~/.local/share/nvim/mason/bin/stylua ]; then
+    ~/.local/share/nvim/mason/bin/stylua ~/.dotfiles/nvim/lua/chadrc.lua
+  fi
 fi
