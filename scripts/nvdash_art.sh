01188c96a3ba6a2f632684443d3d66da7b940375
moucheh <moucheh@proton.me>
2025-09-20 21:55:09 +0200

    updated nvdash_art script to use relative paths

--- scripts/nvdash_art.sh
+++ scripts/nvdash_art.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
-figlet -f ~/.dotfiles/scripts/ansi_shadow.flf $1 > ascii_art.txt
+
+DOTFILES=$2
+DOTFILES=${DOTFILES:="$HOME/.dotfiles"}
+
+SCRIPTS=$DOTFILES/scripts
+
+figlet -f $SCRIPTS/ansi_shadow.flf $1 > ascii_art.txt
 
 sed -i '1i\' ascii_art.txt
 
@@ -9,18 +15,18 @@ sed -i '1i header = {' ascii_art.txt
 
 sed -i '$a },\n' ascii_art.txt
 
-cp ~/.dotfiles/config/nvim/lua/chadrc.lua ~/.dotfiles/config/nvim/lua/chadrc.lua.bak
+cp $DOTFILES/config/nvim/lua/chadrc.lua $DOTFILES/config/nvim/lua/chadrc.lua.bak
 
-printf "\nchadrc.lua file backed up to ~/.dotfiles/config/nvim/chadrc.lua.bak\n"
+printf "\nchadrc.lua file backed up to $DOTFILES/config/nvim/chadrc.lua.bak\n"
 
-sed -i '/header = {/,/},/d' ~/.dotfiles/config/nvim/lua/chadrc.lua
+sed -i '/header = {/,/},/d' $DOTFILES/config/nvim/lua/chadrc.lua
 
-sed -i '/M.nvdash = {/r ascii_art.txt' ~/.dotfiles/config/nvim/lua/chadrc.lua
+sed -i '/M.nvdash = {/r ascii_art.txt' $DOTFILES/config/nvim/lua/chadrc.lua
 
 if command -v stylua >/dev/null 2>&1; then
-  stylua ~/.dotfiles/config/nvim/lua/chadrc.lua
+  stylua $DOTFILES/config/nvim/lua/chadrc.lua
 else
   if [ -f ~/.local/share/nvim/mason/bin/stylua ]; then
-    ~/.local/share/nvim/mason/bin/stylua ~/.dotfiles/config/nvim/lua/chadrc.lua
+    $HOME/.local/share/nvim/mason/bin/stylua $DOTFILES/config/nvim/lua/chadrc.lua
   fi
 fi
