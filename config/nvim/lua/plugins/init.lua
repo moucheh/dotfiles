28493d7ada52d037269cee2edff08349d2cf3b8c
moucheh <moucheh@proton.me>
2025-09-25 00:30:27 +0200

    reintroduced nvim-lspconfig plugin and added lazydev plugin

--- config/nvim/lua/plugins/init.lua
+++ config/nvim/lua/plugins/init.lua
@@ -18,9 +18,25 @@ return {
   },
 
   {
+    'neovim/nvim-lspconfig',
+  },
+
+  {
+    'folke/lazydev.nvim',
+    ft = 'lua', -- only load on lua files
+    opts = {
+      library = {
+        -- See the configuration section for more details
+        -- Load luvit types when the `vim.uv` word is found
+        { path = '${3rd}/luv/library', words = { 'vim%.uv' } },
+      },
+    },
+  },
+
+  {
     'stevearc/oil.nvim',
     opts = {},
-    dependencies = { "nvim-tree/nvim-web-devicons" },
+    dependencies = { 'nvim-tree/nvim-web-devicons' },
     lazy = false,
   },
 }
