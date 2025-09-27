d5c69428f60f876b4e65ea7bbdb9efd853d82123
moucheh <moucheh@proton.me>
2025-09-28 00:02:25 +0200

    netrw .dotfiles are hidden by default

--- config/mvim/lua/netrw_icons.lua
+++ config/mvim/lua/netrw_icons.lua
@@ -1,6 +1,11 @@
 vim.g.netrw_banner = 0
-vim.g.netrw_list_hide = [[\v^\./|^\.\./]]
+
 vim.g.netrw_hide = 1
+vim.g.netrw_list_hide = [[\v^\./|^\.\./]]
+
+local ghregex=[[\(^\|\s\s\)\zs\.\S\+]]
+
+vim.g.netrw_list_hide = vim.g.netrw_list_hide .. ',' .. ghregex
 
 local icons = {
   lua = "",
