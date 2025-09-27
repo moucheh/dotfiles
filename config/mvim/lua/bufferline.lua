871c73f3635fe83d08f32965c13bb5a1546e71bf
moucheh <moucheh@proton.me>
2025-09-27 23:05:36 +0200

    added mouse clickable buttons to buffers, close buffer confirm function and force close bind

--- config/mvim/lua/bufferline.lua
+++ config/mvim/lua/bufferline.lua
@@ -24,11 +24,58 @@ function _G.BufferLine()
       local is_current = bufnr == vim.api.nvim_get_current_buf()
       local is_modified = vim.api.nvim_buf_get_option(bufnr, "modified")
 
-      local label = string.format(" %s %s%s ", icon, short, is_modified and " ●" or "")
+      s = s .. '%' .. bufnr .. '@v:lua.SwitchBuffer@'
 
-      s = s .. (is_current and '%#TabLineSel#' or '%#TabLine#') .. label
+      s = s .. (is_current and '%#TabLineSel#' or '%#TabLine#')
+
+      local label = string.format(" %s %s%s ", icon, short, is_modified and " ●" or "")
+      s = s .. label
+
+      s = s .. '%' .. bufnr .. '@v:lua.CloseBuffer@'
+      s = s .. ' × '
+      s = s .. '%X'
+
+      s = s .. '%X'
     end
   end
+
   s = s .. '%#TabLineFill#'
   return s
 end
+
+function _G.SwitchBuffer(bufnr, _)
+  bufnr = tonumber(bufnr)
+  if vim.api.nvim_buf_is_valid(bufnr) then
+    vim.api.nvim_set_current_buf(bufnr)
+  end
+end
+
+function _G.CloseBuffer(bufnr, _)
+  bufnr = tonumber(bufnr)
+  if not vim.api.nvim_buf_is_valid(bufnr) then return end
+
+  local is_modified = vim.api.nvim_buf_get_option(bufnr, 'modified')
+  local force = false
+
+  if is_modified then
+    local shortname = vim.fn.fnamemodify(vim.api.nvim_buf_get_name(bufnr), ":t")
+    local choice = vim.fn.confirm("Buffer '" .. shortname .. "' has unsaved changes. Close it anyway?", "&Yes\n&No", 2)
+    if choice ~= 1 then
+      return
+    end
+    force = true
+  end
+
+  vim.api.nvim_buf_delete(bufnr, { force = force })
+end
+
+function CloseCurrentBuffer()
+  local bufnr = vim.api.nvim_get_current_buf()
+  _G.CloseBuffer(bufnr)
+end
+
+vim.keymap.set('n', '<leader>x', function()
+  CloseCurrentBuffer()
+end)
+
+vim.keymap.set('n', '<leader>X', ':bdelete!<CR>')
