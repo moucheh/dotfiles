d8a0a24edffc248f9be33ed19de87921b774f017
moucheh <moucheh@proton.me>
2025-09-27 20:45:05 +0200

    disabled providers global variable

--- config/mvim/init.lua
+++ config/mvim/init.lua
@@ -6,6 +6,9 @@ package.path = package.path .. ";" .. config_path .. "/lua/?.lua"
 package.path = package.path .. ";" .. config_path .. "/lsp/?.lua"
 
 vim.g.netrw_banner = 0
+vim.g.loaded_ruby_provider = 0
+vim.g.loaded_python3_provider = 0
+vim.g.loaded_perl_provider = 0
 
 require('statusline')
 require('opts')
