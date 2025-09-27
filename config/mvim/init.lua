69ffdebe930c2661dd291794a8b3fe3883eb7456
moucheh <moucheh@proton.me>
2025-09-27 21:11:26 +0200

    added . and .. to hidden files in netrw, changed colorscheme to sorbet

--- config/mvim/init.lua
+++ config/mvim/init.lua
@@ -1,3 +1,4 @@
+vim.cmd.colorscheme('sorbet')
 vim.cmd('hi normal guibg=none ctermbg=none')
 vim.cmd('hi tablinefill guibg=none ctermbg=none')
 
@@ -6,6 +7,8 @@ package.path = package.path .. ";" .. config_path .. "/lua/?.lua"
 package.path = package.path .. ";" .. config_path .. "/lsp/?.lua"
 
 vim.g.netrw_banner = 0
+vim.g.netrw_list_hide = [[\v^\./|^\.\./]]
+vim.g.netrw_hide = 1
 vim.g.loaded_ruby_provider = 0
 vim.g.loaded_python3_provider = 0
 vim.g.loaded_perl_provider = 0
