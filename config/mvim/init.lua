34302428690ef31be5b82fe15fcf864187d645c1
moucheh <moucheh@proton.me>
2025-09-28 15:19:34 +0200

    extracted theming to its own file

--- config/mvim/init.lua
+++ config/mvim/init.lua
@@ -1,27 +1,3 @@
-vim.api.nvim_create_autocmd('ColorScheme', {
-  callback = function(_)
-    vim.cmd [[
-      hi normal guibg=none ctermbg=none
-
-      hi TabLine     guifg=Gray      guibg=none     gui=none   ctermfg=Gray     ctermbg=none   cterm=none
-      hi TabLineSel  guifg=White     guibg=none     gui=bold   ctermfg=White    ctermbg=none   cterm=bold
-      hi TabLineFill guifg=Gray      guibg=none     gui=none   ctermfg=Gray     ctermbg=none   cterm=none
-
-      hi Pmenu       guifg=Gray      guibg=Black    gui=none   ctermfg=DarkGray ctermbg=Black  cterm=none
-      hi PmenuSel    guifg=White     guibg=Black    gui=bold   ctermfg=White    ctermbg=Black  cterm=bold
-      hi PmenuSbar                   guibg=Black    gui=none                    ctermbg=Black  cterm=none
-      hi PmenuThumb                  guibg=White    gui=none                    ctermbg=White  cterm=none
-
-      hi WildMenu    guifg=LightCyan guibg=Black    gui=bold   ctermfg=cyan     ctermbg=black
-
-      hi! link NormalFloat Normal
-      hi! link FloatBorder LineNr
-    ]]
-  end,
-})
-
-vim.cmd.colorscheme('sorbet')
-
 local config_path = vim.fn.expand('~/.config/mvim')
 package.path = package.path .. ";" .. config_path .. "/lua/?.lua"
 package.path = package.path .. ";" .. config_path .. "/lsp/?.lua"
@@ -30,6 +6,7 @@ vim.g.loaded_ruby_provider = 0
 vim.g.loaded_python3_provider = 0
 vim.g.loaded_perl_provider = 0
 
+require('theme')
 require('statusline')
 require('bufferline')
 require('opts')
