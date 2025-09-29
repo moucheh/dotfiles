92b0bd82924caaf0427c6ea13515809366300526
moucheh <moucheh@proton.me>
2025-09-30 00:01:40 +0200

    removed comments

--- config/nvim/lua/mappings.lua
+++ config/nvim/lua/mappings.lua
@@ -21,13 +21,11 @@ map({ 'n', 'i', 'v' }, '<leader>dn', '<cmd> NoiceDismiss <cr>', { desc = 'dismis
 
 map('n', '-', '<CMD>Oil<CR>', { desc = 'Open parent directory' })
 
--- Center screen when jumping
 map('n', 'n', 'nzzzv', { desc = 'Next search result (centered)' })
 map('n', 'N', 'Nzzzv', { desc = 'Previous search result (centered)' })
 map('n', '<C-d>', '<C-d>zz', { desc = 'Half page down (centered)' })
 map('n', '<C-u>', '<C-u>zz', { desc = 'Half page up (centered)' })
 
--- Splitting & Resizing
 map('n', '<leader>sv', ':vsplit<CR>', { desc = 'Split window vertically' })
 map('n', '<leader>sh', ':split<CR>', { desc = 'Split window horizontally' })
 map('n', '<C-Up>', ':resize +2<CR>', { desc = 'Increase window height' })
@@ -35,17 +33,14 @@ map('n', '<C-Down>', ':resize -2<CR>', { desc = 'Decrease window height' })
 map('n', '<C-Left>', ':vertical resize -2<CR>', { desc = 'Decrease window width' })
 map('n', '<C-Right>', ':vertical resize +2<CR>', { desc = 'Increase window width' })
 
--- Move lines up/down
 map('n', '<A-j>', ':m .+1<CR>==', { desc = 'Move line down' })
 map('n', '<A-k>', ':m .-2<CR>==', { desc = 'Move line up' })
 map('v', '<A-j>', ":m '>+1<CR>gv=gv", { desc = 'Move selection down' })
 map('v', '<A-k>', ":m '<-2<CR>gv=gv", { desc = 'Move selection up' })
 
--- Better indenting in visual mode
 map('v', '<', '<gv', { desc = 'Indent left and reselect' })
 map('v', '>', '>gv', { desc = 'Indent right and reselect' })
 
--- Better J behavior
 map('n', 'J', 'mzJ`z', { desc = 'Join lines and keep cursor position' })
 
 map('n', '<leader>tr', ':Telescope registers<CR>', { desc = 'Show registers in a Telescope panel' })
--- config/nvim/lua/options.lua
+++ config/nvim/lua/options.lua
@@ -4,69 +4,66 @@ vim.wo.relativenumber = true
 
 local o = vim.o
 local opt = vim.opt
-o.cursorlineopt = 'both' -- to enable cursorline!
+o.cursorlineopt = 'both'
 
-opt.number = true -- Line numbers
-opt.relativenumber = true -- Relative line numbers
-opt.cursorline = true -- Highlight current line
-opt.wrap = true -- Wrap lines
-opt.scrolloff = 10 -- Keep 10 lines above/below cursor
-opt.sidescrolloff = 8 -- Keep 8 columns left/right of cursor
+opt.number = true
+opt.relativenumber = true
+opt.cursorline = true
+opt.wrap = true
+opt.scrolloff = 10
+opt.sidescrolloff = 8
 opt.winborder = 'rounded'
 
--- Indentation
-opt.tabstop = 2 -- Tab width
-opt.shiftwidth = 2 -- Indent width
-opt.softtabstop = 2 -- Soft tab stop
-opt.expandtab = true -- Use spaces instead of tabs
-opt.smartindent = true -- Smart auto-indenting
-opt.autoindent = true -- Copy indent from current line
 
--- Search settings
-opt.ignorecase = true -- Case insensitive search
-opt.smartcase = true -- Case sensitive if uppercase in search
-opt.hlsearch = false -- Don't highlight search results
-opt.incsearch = true -- Show matches as you type
+opt.tabstop = 2
+opt.shiftwidth = 2
+opt.softtabstop = 2
+opt.expandtab = true
+opt.smartindent = true
+opt.autoindent = true
 
--- Visual settings
-opt.termguicolors = true -- Enable 24-bit colors
-opt.showmatch = true -- Highlight matching brackets
-opt.matchtime = 2 -- How long to show matching bracket
-opt.cmdheight = 0 -- Command line height
-opt.completeopt = 'menuone,noinsert,noselect' -- Completion options
-opt.showmode = false -- Don't show mode in command line
-opt.pumheight = 10 -- Popup menu height
-opt.pumblend = 10 -- Popup menu transparency
-opt.winblend = 0 -- Floating window transparency
-opt.conceallevel = 0 -- Don't hide markup
-opt.concealcursor = '' -- Don't hide cursor line markup
-opt.synmaxcol = 300 -- Syntax highlighting limit
 
--- File handling
+opt.ignorecase = true
+opt.smartcase = true
+opt.hlsearch = false
+opt.incsearch = true
+
+
+opt.termguicolors = true
+opt.showmatch = true
+opt.matchtime = 2
+opt.cmdheight = 0
+opt.completeopt = 'menuone,noinsert,noselect'
+opt.showmode = false
+opt.pumheight = 10
+opt.pumblend = 10
+opt.winblend = 0
+opt.conceallevel = 0
+opt.concealcursor = ''
+opt.synmaxcol = 300
+
+
 opt.swapfile = false
-opt.undofile = true -- Persistent undo
-opt.undodir = vim.fn.expand '~/.vim/undodir' -- Undo directory
-opt.updatetime = 300 -- Faster completion
-opt.timeoutlen = 500 -- Key timeout duration
-opt.ttimeoutlen = 0 -- Key code timeout
-opt.autoread = true -- Auto reload files changed outside vim
-opt.autowrite = false -- Don't auto save
+opt.undofile = true
+opt.undodir = vim.fn.expand '~/.vim/undodir'
+opt.updatetime = 300
+opt.timeoutlen = 500
+opt.autoread = true
+opt.autowrite = false
 
--- Behavior settings
-opt.hidden = true -- Allow hidden buffers
-opt.errorbells = false -- No error bells
-opt.backspace = 'indent,eol,start' -- Better backspace behavior
-opt.autochdir = false -- Don't auto change directory
-opt.iskeyword:append '-' -- Treat dash as part of word
-opt.path:append '**' -- include subdirectories in search
-opt.mouse = 'a' -- Enable mouse support
-opt.clipboard:append 'unnamedplus' -- Use system clipboard
-opt.modifiable = true -- Allow buffer modifications
-opt.encoding = 'UTF-8' -- Set encoding
 
--- Split behavior
-opt.splitbelow = true -- Horizontal splits go below
-opt.splitright = true -- Vertical splits go right
+opt.hidden = true
+opt.errorbells = false
+opt.backspace = 'indent,eol,start'
+opt.autochdir = false
+opt.iskeyword:append '-'
+opt.path:append '**'
+opt.mouse = 'a'
+opt.clipboard:append 'unnamedplus'
+opt.encoding = 'UTF-8'
+
+opt.splitbelow = true
+opt.splitright = true
 
 vim.g.neovide_opacity = 0.9
 o.guifont = 'CaskaydiaCove Nerd Font:h12'
