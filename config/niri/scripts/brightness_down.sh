5a91d8c63a637a9cdfc54e61580bc80ce51999f3
moucheh <moucheh@proton.me>
2025-10-31 08:39:35 +0100

    added brightness up/down scripts

---
+++ config/niri/scripts/brightness_down.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+brightnessctl --class=backlight set 5%-
+PERCENTAGE=$(brightnessctl -P g)
+
+notify-send -e -h string:x-canonical-private-synchronous:brightness \
+    -h "int:value:$PERCENTAGE" -t 800 "Brightness: ${PERCENTAGE}%"
