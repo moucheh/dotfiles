012ff1dfe0ea8a35fbe7beeb4df972efedffb709
moucheh <moucheh@proton.me>
2025-11-01 00:18:23 +0100

    added hyprland install script

---
+++ scripts/install/hyprland.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+read -n1 -p "Do you want to install Hyprland and all of its dependencies with dnf as well? (requires sudo) [Y/n]: " doit
+case $doit in
+  y|Y|'')
+    echo;
+      sudo dnf install hyprland waybar fuzzel mate-polkit swaybg swaylock-effects wlsunset clipman dunst blueman-applet nm-applet  wl-clipboard
+      ln -s ~/.dotfiles/config/hyprland ~/.config
+      printf "You can now select hyprland from your login manager as a session\n\n"
+    ;;
+  n|N) printf "\n\nContinuing...\n\n";;
+  *) printf "\n\nInvalid option\n\n";;
+esac
