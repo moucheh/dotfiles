73e538aef2165fe3ef1b3d2dc004f8251f580c22
moucheh <moucheh@proton.me>
2025-11-15 20:36:33 +0100

    removing hyprland config

--- config/hypr/brightness.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-PERCENTAGE=$(brightnessctl -P g)
-
-notify-send -e -h string:x-canonical-private-synchronous:brightness \
-    -h "int:value:$PERCENTAGE" -t 800 "Brightness: ${PERCENTAGE}%"
