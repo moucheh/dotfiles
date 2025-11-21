a35c537ac6d25ea09994bec5b8c884604bf734b6
moucheh <moucheh@proton.me>
2025-11-21 10:12:26 +0100

    removed unused scripts

--- config/niri/scripts/brightness_down.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-brightnessctl --class=backlight set 5%-
-PERCENTAGE=$(brightnessctl -P g)
-
-notify-send -e -h string:x-canonical-private-synchronous:brightness \
-    -h "int:value:$PERCENTAGE" -t 800 "Brightness: ${PERCENTAGE}%"
