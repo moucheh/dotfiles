521af8f10f703b7d26e8555ca282e87458b29f72
moucheh <moucheh@proton.me>
2026-04-13 09:22:06 +0200

    updated nvim

--- config/nvim/lua/plugins/treesitter.lua
+++ config/nvim/lua/plugins/treesitter.lua
@@ -5,20 +5,6 @@ end)
 
 return {
   'nvim-treesitter/nvim-treesitter',
-  event = { 'BufReadPost', 'BufNewFile' },
-  cmd = { 'TSInstall', 'TSBufEnable', 'TSBufDisable', 'TSModuleInfo' },
+  lazy = false,
   build = ':TSUpdate',
-  opts = {
-    ensure_installed = { 'lua', 'luadoc', 'printf', 'vim', 'vimdoc', 'html', 'css' },
-
-    highlight = {
-      enable = true,
-      use_languagetree = true,
-    },
-
-    indent = { enable = true },
-  },
-  config = function(_, opts)
-    require('nvim-treesitter.configs').setup(opts)
-  end,
 }
