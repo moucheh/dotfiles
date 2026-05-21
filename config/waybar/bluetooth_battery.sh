5379aabb4c952b942ad23da6284dba9a468571fc
moucheh <moucheh@proton.me>
2026-05-22 01:03:11 +0200

    removed bluetooth_battery module from waybar

--- config/waybar/bluetooth_battery.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-if ! [ -f /tmp/bluetooth_volume ]; then
-  touch /tmp/bluetooth_battery
-fi
-
-echo "$(upower -i $(upower -e | grep headset) | grep percentage | awk '{print $2}')" >/tmp/bluetooth_battery
-
-cat /tmp/bluetooth_battery
